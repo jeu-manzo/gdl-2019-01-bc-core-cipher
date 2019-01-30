58facc29dc205730df63e409b1bfa4a42f1deb50
Jeu <jeunettemanzo@gmail.com>
2019-01-30 10:45:31 -0600

    transferencia de archivos

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,15 @@
 window.cipher = {
-  // ... 
+  //Función para encriptar
+  function encode(x, n) {
+    let formulaEncode = (x - 65 + n) % 26 + 65
+    return formulaEncode
+  };
+
+
+  //Función para descifrar
+  function decode(x, n) {
+    let formulaDecode = (x + 65 - n) % 26 + 65
+    return formulaDecode
+  };
+
 };
---
+++ src/index.js
@@ -0,0 +1,47 @@
+const mainScreen = document.getElementById('screen-main');
+const resultsScreen = document.getElementById('screen-results');
+
+//funcion que recibe un array y lo itera (para el display)
+function display (elements, display = 'inline-block') {
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].style.display = display
+  }
+};
+
+//Ocultar display resultsScreen
+display([resultsScreen], "none");
+
+
+
+let position = "";
+
+const inputPosition = document.getElementById('input-position');
+
+inputPosition.addEventListener("keyup", function(event) {
+  position = event.target.value
+  // console.log(position)
+  // if (event.keyCode === "click") {
+  //    // Crear la acción del botón con el click
+  //    document.getElementById('main-button-decipher').click();
+  //    document.getElementById('main-button-encrypt').click();
+  // }
+});
+
+
+document.getElementById("main-button-decipher").addEventListener("click", buttonDecipher);
+function buttonDecipher() {
+    // si pasa la validacion
+    if (position.length > 1) {
+      display([mainScreen], "none")
+      display([resultsScreen])
+    }
+};
+
+document.getElementById("main-button-encrypt").addEventListener("click", buttonEncrypt);
+function buttonEncrypt() {
+    // si pasa la validacion
+    if (position.length > 1) {
+      display([mainScreen], "none")
+      display([resultsScreen])
+    }
+};
