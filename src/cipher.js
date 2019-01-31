80f2ccc1534ee22d8c638e5a17ed3c918a4d165a
Jeu <jeunettemanzo@gmail.com>
2019-01-31 13:17:04 -0600

    cipher.js terminado

--- src/cipher.js
+++ src/cipher.js
@@ -1,50 +1,118 @@
-// window.cipher = {
-//   //Función para encriptar
-//   function encode(x, n) {
-//     let formulaEncode = (x - 65 + n) % 26 + 65
-//     return formulaEncode
-//   };
-//
-//
-//   //Función para descifrar
-//   function decode(x, n) {
-//     let formulaDecode = (x + 65 - n) % 26 + 65
-//     return formulaDecode
-//   };
-//
-// };
+ window.cipher = {
+   encode: function passStrToEncoded (str, n) {
+     const str = 'Hola como estas'
+     const n = 5
+     const strUpper = str.toUpperCase(); // Convertir cadena de texto en Mayúsculas
+     const splited = strUpper.split(' '); // ['hola', 'como', 'estas']
+     const result = []; // Variable que guardará un array que contendrá cada palabra ya codificada y convertida en letra humana
 
 
-const str = 'hola k ase'
-const n = 5
+     //Bucle que pasara por cada índice del array splited (cada palabra)
+     for (let i = 0; i < splited.length; i++) {
+       let word = splited[i];
+       //Bucle que pasara por cada índice de cada cadena de el índice anterior (cada letra de cada palabra)
+       for (let j = 0; j < word.length; j++) {
+         let singleChart = word[j]; //Variable para arrojar cada letra
+         let chartAscii = singleChart.charCodeAt(0); //Convertir cada letra en Ascii
+         let encoded = (chartAscii - 65 + n) % 26 + 65; //Encriptar cada #Ascii con la fórmula
+         let encodedToChar = String.fromCharCode(encoded); //Convertir cada #Ascii Encriptado a letra
+         result.push(encodedToChar); //Ingresar al array result cada palabra ya codificada y convertida a letra
+       }
 
-function passStrToEncoded (str, n) {
-  const strUpper = str.toUpperCase()
-  const splited = strUpper.split(' ') // ['hola', 'k', 'ase']
-  const result = []
+       result.push(' '); //Agregar un espacio entre cada palabra del array result
+     }
 
-  for (let i = 0; i < splited.length; i++) {
-    let word = splited[i]
+     return result.join(''); //Unir cada índice del array (cada palabra) con un espacio
+   },
 
-    for (let j = 0; j < word.length; j++) {
-      let singleChart = word[j]
-      let chartAscl = singleChart.charCodeAt(0)
-      let encoded = encode(chartAscl, n)
-      let encodedToChar = String.fromCharCode(encoded)
-      result.push(encodedToChar)
-    }
+   decode: function passStrToDecoded(str, n) {
+     const str = 'Hola como estas'
+     const n = 5
+     const strUpper = str.toUpperCase(); // Convertir cadena de texto en Mayúsculas
+     const splited = strUpper.split(' '); // ['hola', 'como', 'estas']
+     const result = []; // Variable que guardará un array que contendrá cada palabra ya codificada y convertida en letra humana
 
-    result.push(' ')
-  }
 
-  return result.join('')
-}
+     //Bucle que pasara por cada índice del array splited (cada palabra)
+     for (let i = 0; i < splited.length; i++) {
+       let word = splited[i];
+       //Bucle que pasara por cada índice de cada cadena de el índice anterior (cada letra de cada palabra)
+       for (let j = 0; j < word.length; j++) {
+         let singleChart = word[j]; //Variable para arrojar cada letra
+         let chartAscii = singleChart.charCodeAt(0); //Convertir cada letra en Ascii
+         let encoded = (chartAscii + 65 - n) % 26 + 65; //Encriptar cada #Ascii con la fórmula
+         let encodedToChar = String.fromCharCode(encoded); //Convertir cada #Ascii Encriptado a letra
+         result.push(encodedToChar); //Ingresar al array result cada palabra ya codificada y convertida a letra
+       }
 
-function encode(x, n) {
-  const formulaEncode = (x - 65 + n) % 26 + 65
-  return formulaEncode
+       result.push(' '); //Agregar un espacio entre cada palabra del array result
+     }
+
+     return result.join(''); //Unir cada índice del array (cada palabra) con un espacio
+   },
+
 };
 
-const coded = passStrToEncoded(str, n)
-
-coded
+//
+// const str = 'Hola como estas'
+// const n = 5
+//
+// function passStrToEncoded (str, n) {
+//
+//   const strUpper = str.toUpperCase() // Convertir cadena de texto en Mayúsculas
+//   const splited = strUpper.split(' ') // ['hola', 'como', 'estas']
+//   const result = [] // Variable que guardará un array que contendrá cada palabra ya codificada y convertida en letra humana
+//
+//
+//   //Bucle que pasara por cada índice del array splited (cada palabra)
+//   for (let i = 0; i < splited.length; i++) {
+//     let word = splited[i]
+//     //Bucle que pasara por cada índice de cada cadena de el índice anterior (cada letra de cada palabra)
+//     for (let j = 0; j < word.length; j++) {
+//       let singleChart = word[j] //Variable para arrojar cada letra
+//       let chartAscii = singleChart.charCodeAt(0) //Convertir cada letra en Ascii
+//       let encoded = (chartAscii - 65 + n) % 26 + 65 //Encriptar cada #Ascii con la fórmula
+//       let encodedToChar = String.fromCharCode(encoded) //Convertir cada #Ascii Encriptado a letra
+//       result.push(encodedToChar) //Ingresar al array result cada palabra ya codificada y convertida a letra
+//     }
+//
+//     result.push(' ') //Agregar un espacio entre cada palabra del array result
+//   }
+//
+//   return result.join('') //Unir cada índice del array (cada palabra) con un espacio
+//
+// };
+//
+// const coded = passStrToEncoded(str, n) //
+//
+// coded
+//
+//
+// function passStrToDecoded(str, n) {
+//
+//   const strUpper = str.toUpperCase() // Convertir cadena de texto en Mayúsculas
+//   const splited = strUpper.split(' ') // ['hola', 'como', 'estas']
+//   const result = [] // Variable que guardará un array que contendrá cada palabra ya codificada y convertida en letra humana
+//
+//
+//   //Bucle que pasara por cada índice del array splited (cada palabra)
+//   for (let i = 0; i < splited.length; i++) {
+//     let word = splited[i]
+//     //Bucle que pasara por cada índice de cada cadena de el índice anterior (cada letra de cada palabra)
+//     for (let j = 0; j < word.length; j++) {
+//       let singleChart = word[j] //Variable para arrojar cada letra
+//       let chartAscii = singleChart.charCodeAt(0) //Convertir cada letra en Ascii
+//       let encoded = (chartAscii + 65 - n) % 26 + 65 //Encriptar cada #Ascii con la fórmula
+//       let encodedToChar = String.fromCharCode(encoded) //Convertir cada #Ascii Encriptado a letra
+//       result.push(encodedToChar) //Ingresar al array result cada palabra ya codificada y convertida a letra
+//     }
+//
+//     result.push(' ') //Agregar un espacio entre cada palabra del array result
+//   }
+//
+//   return result.join('') //Unir cada índice del array (cada palabra) con un espacio
+// };
+//
+// const decoded = passStrToDecoded(str, n) //
+//
+// decoded
--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ const resultsScreen = document.getElementById('screen-results');
 //funcion que recibe un array y lo itera (para el display)
 function display (elements, display = 'inline-block') {
   for (let i = 0; i < elements.length; i++) {
-    elements[i].style.display = display
+    elements[i].style.display = display;
   }
-};
+}
 
 //Ocultar display resultsScreen
 display([resultsScreen], "none");
@@ -18,7 +18,7 @@ let position = "";
 const inputPosition = document.getElementById('input-position');
 
 inputPosition.addEventListener("keyup", function(event) {
-  position = event.target.value
+  position = event.target.value;
 });
 
 
@@ -26,16 +26,16 @@ document.getElementById("main-button-decipher").addEventListener("click", button
 function buttonDecipher() {
     // si pasa la validacion
     if (position.length > 1) {
-      display([mainScreen], "none")
-      display([resultsScreen])
+      display([mainScreen], "none");
+      display([resultsScreen]);
     }
-};
+}
 
 document.getElementById("main-button-encrypt").addEventListener("click", buttonEncrypt);
 function buttonEncrypt() {
     // si pasa la validacion
     if (position.length > 1) {
-      display([mainScreen], "none")
-      display([resultsScreen])
+      display([mainScreen], "none");
+      display([resultsScreen]);
     }
-};
+}
